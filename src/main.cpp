70cf1a134dff5a43e2c12539bf36efbe05e38e0a
Mathieu PALOMBA <mathieupalomba@msn.com>
2013-09-29 11:18:57 +0200

    Test item movable in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,34 @@
 #include <QtGui/QApplication>
 #include "window/mainwindow.h"
-
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    QGraphicsScene scene;
+    //scene.setForegroundBrush(QBrush(Qt::blue));
+    //scene.setBackgroundBrush(QBrush(Qt::red));
+
+    QGraphicsRectItem* rectItem = scene.addRect(QRectF(0,0,100,100));
+    rectItem->setFlag(QGraphicsItem::ItemIsMovable);    // To accept movable shape
+
+    QPen pen;
+    pen.setColor(Qt::red);
+    pen.setStyle(Qt::DashDotLine);
+
+    QBrush brush;
+    brush.setStyle(Qt::SolidPattern);   // To allow fill out color
+    brush.setColor(Qt::green);          // Fill out color
+
+    rectItem->setPen(pen);              // Apply outline color
+    rectItem->setBrush(brush);          // Apply fill out color
+
+    QGraphicsView view(&scene);
+    view.show();
 
     return a.exec();
 }
